d161ccc1be887575fd1d3d98cd53381b52e07b7a
ngonzalespalacios <noelia.gonzales@estudiante.ucsm.edu.pe>
2025-04-29 16:36:47 -0500

    Se modifica la clase Nodo

--- src/Actividad/Nodo.java
+++ src/Actividad/Nodo.java
@@ -1,13 +1,12 @@
 package Actividad;
 
 public class Nodo <T>{
-    private T dato;
-    private Nodo<T> next;
+    public T dato;
+    public Nodo<T> next;
 
     public Nodo(T dato){
         this.dato=dato;
         this.next=null;
     }
-
 
 }
