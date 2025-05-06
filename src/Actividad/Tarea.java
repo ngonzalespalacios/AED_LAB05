2bd2977c23a1e264a76733cf8aff3093f95bffb2
ngonzalespalacios <noelia.gonzales@estudiante.ucsm.edu.pe>
2025-05-06 10:31:08 -0500

    Se crea la clase tarea con el constructor, getters y toSRING

---
+++ src/Actividad/Tarea.java
@@ -0,0 +1,23 @@
+package Actividad;
+public class Tarea {
+    private String titulo;
+    private int prioridad;
+
+    public Tarea(String titulo, int prioridad) {
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" + "titulo='" + titulo + '\'' + ", prioridad=" + prioridad + '}';
+    }
+}
