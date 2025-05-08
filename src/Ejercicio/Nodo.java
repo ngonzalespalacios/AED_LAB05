3ee63fee783d0a69ac406fe80a09a8baf86d823d
ngonzalespalacios <noelia.gonzales@estudiante.ucsm.edu.pe>
2025-05-08 17:17:41 -0500

    Se crea el paquete ejercicios y se copian las clases nodo y tarea que son las clases básicas

---
+++ src/Ejercicio/Nodo.java
@@ -0,0 +1,24 @@
+package Ejercicio;
+
+public class Nodo <T>{
+    private T dato;
+    private Nodo<T> next;
+
+    public Nodo(T dato){
+        this.dato=dato;
+        this.next=null;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+    public Nodo<T> getNext() {
+        return next;
+    }
+    public void setDato(T dato){
+        this.dato=dato;
+    }
+    public void setNext(Nodo<T> next) {
+        this.next = next;
+    }
+}
